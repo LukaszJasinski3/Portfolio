b3cbce0b7d3e0ef12849512099742e2a5088951b
Lukasz J <lukas.jasinski3@gmail.com>
2022-02-23 19:51:49 +0000

    Initial commit

---
+++ displayNav.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("extra-pages");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }  
