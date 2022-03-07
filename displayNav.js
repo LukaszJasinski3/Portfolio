35377acd50c9dc24bd5b25961570bdbc2797ca2b
Lukasz J <lukas.jasinski3@gmail.com>
2022-03-07 20:05:16 +0000

    Media query test

--- displayNav.js
+++ displayNav.js
@@ -1,8 +1,14 @@
+const width = window.innerWidth;
+
 function myFunction() {
     var x = document.getElementById("extra-pages");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+    if (width < 1024) {
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
     }
-  }  
+
+  }
+  
